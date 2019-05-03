65f55d52a8f1ac59dd213a766db4dde065376e49
LeiYangGH <675686066@qq.com>
2019-05-03 18:32:05 +0800

    grid row cos and listbox

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,77 @@
+using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+
+namespace FeedLabelPrint.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            if (IsInDesignMode)
+            {
+            }
+            else
+            {
+                this.CreateSampleData();
+
+                // Code runs "for real"
+            }
+        }
+
+        private void CreateSampleData()
+        {
+            this.ObsBtwDirs = new ObservableCollection<string>() { "º¶À«¡œ", "”„À«¡œ", "÷ÌÀ«¡œ" };
+            this.ObsBtwFiles = new ObservableCollection<string>() { "111", "222", "333", "444", "555", "666", "777", "888", "999" };
+        }
+
+        private ObservableCollection<string> obsBtwDirs;
+        public ObservableCollection<string> ObsBtwDirs
+        {
+            get
+            {
+                return this.obsBtwDirs;
+            }
+            set
+            {
+                if (this.obsBtwDirs != value)
+                {
+                    this.obsBtwDirs = value;
+                    this.RaisePropertyChanged(nameof(ObsBtwDirs));
+                }
+            }
+        }
+
+        private ObservableCollection<string> obsBtwFiles;
+        public ObservableCollection<string> ObsBtwFiles
+        {
+            get
+            {
+                return this.obsBtwFiles;
+            }
+            set
+            {
+                if (this.obsBtwFiles != value)
+                {
+                    this.obsBtwFiles = value;
+                    this.RaisePropertyChanged(nameof(ObsBtwFiles));
+                }
+            }
+        }
+
+    }
+}
