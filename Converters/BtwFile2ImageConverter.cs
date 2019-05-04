70de5a6baa90eb70c84a9e114c1522b08e498553
LeiYangGH <675686066@qq.com>
2019-05-04 10:04:45 +0800

    can display images in uniformgrid

--- Converters/BtwFile2ImageConverter.cs
+++ Converters/BtwFile2ImageConverter.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using Seagull.BarTender.Print;
+using System;
 using System.IO;
 using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace FeedLabelPrint.Converters
 {
-    [ValueConversion(typeof(string), typeof(string))]
-    public class BtwFile2ImageConverter
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class BtwFile2ImageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter,
             System.Globalization.CultureInfo culture)
         {
-            return Path.GetFileNameWithoutExtension((string)value);
+            string btwFileName = (string)value;
+            System.Drawing.Image btwImage = LabelFormatThumbnail.Create(btwFileName, System.Drawing.Color.Gray,
+   800, 800);
+            using (var ms = new MemoryStream())
+            {
+                btwImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                ms.Position = 0;
+
+                var bi = new BitmapImage();
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = ms;
+                bi.EndInit();
+                btwImage.Dispose(); //if bmp is not used further.
+                return bi;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
